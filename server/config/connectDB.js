0ab77de15203756f11dad48700958e079c487738
MohammedHassan07 <mhassancs260@gmail.com>
2024-10-07 17:58:31 +0530

    FEAT: basic server setup

---
+++ server/config/connectDB.js
@@ -0,0 +1,9 @@
+const mongoose = require('mon')
+const mongoose = require('mongoose')
+module.exports = async function connectDB() {
+
+    const DB_URL = process.env.DB_URL
+    const connection = await mongoose.connect(DB_URL)
+
+    if (connection) console.log(connection)
+}
